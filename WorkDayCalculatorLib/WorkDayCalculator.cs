e6b0e8f8d296f36721dd98b36676677b937cc414
4uva <41539104+4uva@users.noreply.github.com>
2019-04-04 12:57:39 +0300

    removed data copying

--- WorkDayCalculatorLib/WorkDayCalculator.cs
+++ WorkDayCalculatorLib/WorkDayCalculator.cs
@@ -8,6 +8,15 @@ namespace WorkDayCalculatorLib
 {
     static public class WorkDayCalculator
     {
+        private class DateRangeByStartDateComparer : IComparer<DateRange>
+        {
+            public int Compare(DateRange x, DateRange y)
+            {
+                // x and y are never null here
+                return x.StartDate.CompareTo(y.StartDate);
+            }
+        }
+
         static public DateTime Compute(DateTime begin, int duration, params DateRange[] weekends)
         {
             if (begin.TimeOfDay != TimeSpan.Zero)
@@ -18,50 +27,44 @@ namespace WorkDayCalculatorLib
                 throw new ArgumentNullException(nameof(weekends));
 
             int weekendCount = weekends.Length;
-            DateTime[] weekendsStart = new DateTime[weekendCount];
-            DateTime[] workWeekStart = new DateTime[weekendCount];
-            for (int i = 0; i < weekendCount; i++)
-            {
-                weekendsStart[i] = weekends[i].StartDate;
-                workWeekStart[i] = weekends[i].EndDate.AddDays(1);
-            }
 
-            DateTime current;
-            int indexOfCurrentWeekend = Array.BinarySearch(weekendsStart, begin);
-            if (indexOfCurrentWeekend >= 0) // found
-            {
-                // shift to the next working day
-                current = workWeekStart[indexOfCurrentWeekend];
-            }
-            else
-            {
+            var beginRange = new DateRange(begin, begin);
+            int indexOfCurrentWeekend = Array.BinarySearch(weekends, beginRange, new DateRangeByStartDateComparer());
+            if (indexOfCurrentWeekend < 0) // not found
                 indexOfCurrentWeekend = ~indexOfCurrentWeekend - 1; // index of first weekend starting before or -1
-                if (indexOfCurrentWeekend >= 0 && begin < workWeekStart[indexOfCurrentWeekend])
-                    current = workWeekStart[indexOfCurrentWeekend];
-                else
-                    current = begin;
+
+            DateTime currentDate = begin;
+            if (indexOfCurrentWeekend >= 0) // if there is weekend before...
+            {
+                DateRange startWeekend = weekends[indexOfCurrentWeekend];
+                DateTime firstWorkDayAfterStartWeekend = startWeekend.EndDate.AddDays(1);
+                if (currentDate < firstWorkDayAfterStartWeekend) // and that weekend didn't finish yet
+                    currentDate = firstWorkDayAfterStartWeekend; // shift to the next working day
             }
 
             // current is always a working day
             // indexOfCurrentWeekend is before current
             int remainingDays = duration - 1;
             indexOfCurrentWeekend++;
+            // loop over all remaining weekends
             for (/**/; indexOfCurrentWeekend < weekendCount; indexOfCurrentWeekend++)
             {
-                int daysToNextWeekend = (weekendsStart[indexOfCurrentWeekend] - current).Days;
-                if (daysToNextWeekend >= remainingDays)
+                // compute working days on the preceding week
+                DateRange currentWeekend = weekends[indexOfCurrentWeekend];
+                int daysToCurrentWeekend = (currentWeekend.StartDate - currentDate).Days;
+                if (daysToCurrentWeekend >= remainingDays)
                 {
                     // we can finish before weekend
-                    return current.AddDays(remainingDays);
+                    return currentDate.AddDays(remainingDays);
                 }
                 else
                 {
-                    current = workWeekStart[indexOfCurrentWeekend];
-                    remainingDays -= daysToNextWeekend;
+                    currentDate = currentWeekend.EndDate.AddDays(1);
+                    remainingDays -= daysToCurrentWeekend;
                 }
             }
             // no more weekends
-            return current.AddDays(remainingDays);
+            return currentDate.AddDays(remainingDays);
         }
     }
 }
